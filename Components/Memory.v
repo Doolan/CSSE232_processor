80725c4ea8e4fda5ed9c4d3fcec2033d88783fa9
xniccum <xniccum@gmail.com>
2014-10-28 13:52:20 -0400

    reg

---
+++ Components/Memory.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:38:37 10/28/2014 
+// Design Name: 
+// Module Name:    Memory 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Memory(address,write_data,r_flag,w_flag,clk,read_data);
+	input wire [15:0] address;
+	input wire signed [15:0] write_data;
+	input wire r_flag;
+	input wire w_flag;
+	input wire clk;
+	output wire signed [15:0] read_data;
+	reg [15:0] mem[15:0];
+	
+	assign 
+
+endmodule
--- Components/Register_File.v
+++ Components/Register_File.v
@@ -18,19 +18,23 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Register_File(reg1,reg2,write_code,w_flag,w_data,clock,read1,read2);
+module Register_File(reg1,reg2,write_code,w_flag,w_data,clk,read1,read2);
 	input [15:0] reg1;//
 	input [15:0] reg2;//
 	input [15:0] write_code;
 	input w_flag;//
 	input [15:0] w_data;//
-	input clock;//
+	input clk;//
 	output [15:0] read1;
 	output [15:0] read2;
 	reg [15:0] ref_f[15:0];
 	
 	assign read1=ref_f[reg1];
 	assign read2=ref_f[reg2];
-	assign ref_f[write_code]=((clock)&&(w_flag))?16'b0000000000000000:16'b0000000000000001;
+	always@(posedge clk)
+	begin
+		
+	end
+	//assign ref_f[write_code]=((clk)&&(w_flag))?16'b0000000000000000:16'b0000000000000001;
 
 endmodule
