23b93fc2deef634303e656c0e9cb520b79a2b2e3
xniccum <xniccum@gmail.com>
2014-11-05 15:48:04 -0500

    control update

--- Components/state_machine.v
+++ Components/state_machine.v
@@ -59,7 +59,131 @@ module state_machine(ReadAddr,
 	reg PCSource;
 	reg PCWrite;
 	reg RegWrite;
-	reg MemWrite
+	reg MemWrite;
+
+	//states
+	reg [3:0] current_state;
+	reg [3:0] next_state;
+	
+	//handle reset
+	always @ (posedge CLK,posedge Reset)  
+	begin
+		if(Reset)
+			currrent_state=0;
+		else
+			current_state=next_state;
+	end
+	
+	always@(current_state)
+	begin
+		//reset signals
+		
+		case(current_state)
+			0://Fetch
+					begin
+						ReadAddr=0;
+						IRWrite=1;
+						ALUA=0;
+						ALUB=1;
+						ALUControl=1;
+						PCSource=1;
+						PCWrite=1;
+						RegWrite=0;
+						MemWrite=0;
+					end
+					
+			1://decode
+				begin
+					AWrite=1;
+					BWrite=1;
+					PCWriteCond=0;
+					PCWrite=0;
+					RegWrite=0;
+					RegRead=1;
+					MemWrite=0;
+					IRWrite=0;
+				end
+				
+			2://R_Execution
+            begin
+               ALUControl=0;
+					ALUA=1;
+					ALUB=1;
+					ALUOutWrite=1;
+					CSrc=0;
+					RegRead=0;
+					CWrite=1;
+					PCWrite=0;
+					PCWriteCond=0;
+					MemWrite=0;
+					IRWrite=0;
+					//RegWrite=0;
+            end
+        
+         3://R_Write
+            begin
+               RegWrite=1;
+					RegDest=0;
+					MemToReg=0;
+					/*PCWrite=0;
+					PCWriteCond=0;
+					MemWrite=0;
+					IRWrite=0;*/
+            end
+				
+			4://ber
+				begin
+					PCWrite=0;
+					PCWriteCond=1;
+					MemWrite=0;
+					IRWrite=0;
+					RegWrite=0;
+				end
+			
+			5://rst stuff
+				begin
+				
+				end
+				
+			6://rst stuff
+				begin
+				
+				end
+				
+			7://calculate memory
+				begin
+					ALUControl=1;
+					ALUA=1;
+					ALUB=2;
+					ALUOutWrite=1;
+					ShifterInput=0;
+					ShifterControl=2;
+					PCWrite=0;
+					PCWriteCond=1;
+					MemWrite=0;
+					IRWrite=0;
+					RegWrite=0;
+				end
+			
+			8://rst stuff
+				begin
+				
+				end			
+			
+			9://rst stuff
+				begin
+				
+				end
+				
+			10://rst stuff
+				begin
+				
+				end	
+			endcase
+	end
 
 
-	always @ (posedge CLK)  
+
+
+
+
