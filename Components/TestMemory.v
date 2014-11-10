c04c03d785833de9aa8823c84823ca354e927285
Christian Schulz <schulzcc@schulzcc-ThinkPad-W530.(none)>
2014-11-09 21:08:19 -0500

    The test bench has no errors now

--- Components/TestMemory.v
+++ Components/TestMemory.v
@@ -1,42 +1,72 @@
-// Verilog Test Fixture Template
+// Verilog test fixture created from schematic /home/schulzcc/proj232git/CSSE232_processor/Components/Memory.sch - Sun Nov  9 21:01:24 2014
 
-  `timescale 1 ns / 1 ps
+`timescale 1ns / 1ps
 
-  module TEST_gate;
-          reg <signal1>;
-          reg [2:0] <signal2>;
-          wire [3:0] <signal3>;
-          wire <signal4>;
+module Memory_Memory_sch_tb();
 
-          <module_name> <instance_name> (
-                  <port1>,
-                  <port2>
-          );
+// Inputs
+   reg [15:0] Addr;
+   reg [15:0] WriteData;
+   reg WriteFlag;
+   reg [15:0] Input;
+   reg KernelFlag;
+   reg Clk;
+   reg InputRst;
 
-          integer <name1>;
-          integer <name2>;
+// Output
+   wire [15:0] Output;
+   wire AccInv;
+   wire [15:0] ReadData;
+   wire InputRecv;
 
-   // The following code initializes the Global Set Reset (GSR) and Global Three-State (GTS) nets
-   // Refer to the Synthesis and Simulation Design Guide for more information on this process
-   reg GSR;
-   assign glbl.GSR = GSR;
-   reg GTS;
-   assign glbl.GTS = GTS;
+// Bidirs
 
-   initial begin
-      GSR = 1;
-      GTS = 0; // GTS is not activated by default
-      #100; // GSR is set for 100 ns
-      GSR = 0;
-   end
-
-  // Initialize Inputs
-      `define auto_init
-
-      `ifdef auto_init
-
-          initial begin
+// Instantiate the UUT
+   Memory UUT (
+		.Addr(Addr), 
+		.WriteData(WriteData), 
+		.WriteFlag(WriteFlag), 
+		.Input(Input), 
+		.KernelFlag(KernelFlag), 
+		.Clk(Clk), 
+		.Output(Output), 
+		.AccInv(AccInv), 
+		.ReadData(ReadData), 
+		.InputRecv(InputRecv), 
+		.InputRst(InputRst)
+   );
+	
+	parameter   PERIOD = 20;
+   parameter   real DUTY_CYCLE = 0.5;
+   parameter   OFFSET = 10;
+   
+   initial    // Clock process for CLK
+     begin
+        #OFFSET;
+        forever
+          begin
+             Clk = 1'b0;
+             #(PERIOD-(PERIOD*DUTY_CYCLE)) Clk = 1'b1;
+             #(PERIOD*DUTY_CYCLE);
           end
-
-      `endif
-  endmodule
+     end
+	  
+// Initialize Inputs
+   initial begin
+		Addr = 0;
+		WriteData = 0;
+		WriteFlag = 0;
+		Input = 0;
+		KernelFlag = 0;
+		InputRst = 0;
+		#200;
+		// First test: simple write
+		Addr = 4'h4000;
+		WriteData = 4'h1234;
+		WriteFlag = 1;
+		#PERIOD;
+		// Check to make sure everything came out okay
+		WriteFlag = 0;
+		
+	end
+endmodule
